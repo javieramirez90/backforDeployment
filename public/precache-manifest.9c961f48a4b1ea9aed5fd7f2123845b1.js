c782c278631b11619f99dfe80f9631d7b6a1fa6b
javieramirez90 <javier.ramirez@aiesec.net>
2019-03-13 00:44:27 -0600

    Deploying to heroku

---
+++ public/precache-manifest.9c961f48a4b1ea9aed5fd7f2123845b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6864c0c79aa1ec691e18",
+    "url": "/static/js/main.d5f0f1b5.chunk.js"
+  },
+  {
+    "revision": "7b838d4b9080dbb8553c",
+    "url": "/static/js/2.2cf6426a.chunk.js"
+  },
+  {
+    "revision": "6864c0c79aa1ec691e18",
+    "url": "/static/css/main.10abb542.chunk.css"
+  },
+  {
+    "revision": "7b838d4b9080dbb8553c",
+    "url": "/static/css/2.5be00a97.chunk.css"
+  },
+  {
+    "revision": "30c356cea9ac122986f29946dda13f5a",
+    "url": "/index.html"
+  }
+];
